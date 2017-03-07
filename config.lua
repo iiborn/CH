5309c30590ae7213b6d98feced6b580d5e8f1449
iiborn <abbasgit91@gmail.com>
2017-03-07 13:08:13 +0330

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,4 @@
+bot_token = "230413066:AAF8VFv8sP3xQpEXX3BtYx7wh32j4upTUCg" --"199123242:AAHo-jW9ls2zU1kMu3g7UwewEkptZikB_bk"
+send_api = "https://api.telegram.org/bot"..bot_token
+bot_version = "2.2"
+sudo_id = 251862863
