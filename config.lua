b871f8e82f484800786e41f63c46bc66c6d5dd79
iiborn <abbasgit91@gmail.com>
2017-03-15 15:35:58 +0330

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
-bot_token = "230413066:AAF8VFv8sP3xQpEXX3BtYx7wh32j4upTUCg" --"199123242:AAHo-jW9ls2zU1kMu3g7UwewEkptZikB_bk"
+bot_token = "343987737:AAFnG5DD2yZllylAuDa0C3j2ih6dGh-rPyk" --"199123242:AAHo-jW9ls2zU1kMu3g7UwewEkptZikB_bk"
 send_api = "https://api.telegram.org/bot"..bot_token
 bot_version = "2.2"
-sudo_id = 251862863
+sudo_id = 326824638
